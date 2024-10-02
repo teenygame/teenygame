818a4ffca83345382ae17b18fd6a1c185285d04f
coevolutions <156261562+coevolutions@users.noreply.github.com>
2024-10-02 02:22:46 -0700

    dedupe convert_error

--- src/file/native.rs
+++ src/file/native.rs
@@ -2,24 +2,20 @@ use std::io::ErrorKind;
 
 use super::Error;
 
+fn convert_error(e: std::io::Error) -> Error {
+    if e.kind() == ErrorKind::NotFound {
+        Error::NotFound
+    } else {
+        Error::Other(e.into())
+    }
+}
+
 #[cfg(feature = "tokio")]
 pub async fn read(path: &str) -> Result<Vec<u8>, Error> {
-    Ok(tokio::fs::read(path).await.map_err(|e| {
-        if e.kind() == ErrorKind::NotFound {
-            Error::NotFound
-        } else {
-            Error::Other(e.into())
-        }
-    })?)
+    Ok(tokio::fs::read(path).await.map_err(convert_error)?)
 }
 
 #[cfg(not(feature = "tokio"))]
 pub async fn read(path: &str) -> Result<Vec<u8>, Error> {
-    Ok(std::fs::read(path).map_err(|e| {
-        if e.kind() == ErrorKind::NotFound {
-            Error::NotFound
-        } else {
-            Error::Other(e.into())
-        }
-    })?)
+    Ok(std::fs::read(path).map_err(convert_error)?)
 }
