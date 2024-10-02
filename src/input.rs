aa9fde7ec3fe1f86cfd1c70c1c6641743a8bacf9
coevolutions <156261562+coevolutions@users.noreply.github.com>
2024-10-02 12:45:53 -0700

    finger -> contact

--- src/input.rs
+++ src/input.rs
@@ -5,77 +5,77 @@ use winit::dpi::PhysicalPosition;
 pub use winit::{event::MouseButton, keyboard::KeyCode};
 
 #[derive(Clone, Copy, Hash, PartialEq, Eq)]
-pub struct Finger(u64);
+pub struct Contact(u64);
 
 /// Touch state.
 pub struct Touch {
-    last_fingers: HashMap<Finger, PhysicalPosition<f64>>,
-    fingers: HashMap<Finger, PhysicalPosition<f64>>,
-    next_finger_id: u64,
-    ids_to_fingers: HashMap<u64, Finger>,
+    last_contacts: HashMap<Contact, PhysicalPosition<f64>>,
+    contacts: HashMap<Contact, PhysicalPosition<f64>>,
+    next_contact_id: u64,
+    ids_to_contacts: HashMap<u64, Contact>,
 }
 
 impl Touch {
     fn new() -> Self {
         Self {
-            fingers: HashMap::new(),
-            last_fingers: HashMap::new(),
-            next_finger_id: 0,
-            ids_to_fingers: HashMap::new(),
+            contacts: HashMap::new(),
+            last_contacts: HashMap::new(),
+            next_contact_id: 0,
+            ids_to_contacts: HashMap::new(),
         }
     }
 
-    fn next_finger(&mut self) -> Finger {
-        let finger = Finger(self.next_finger_id);
-        self.next_finger_id += 1;
-        finger
+    fn next_contact(&mut self) -> Contact {
+        let contact = Contact(self.next_contact_id);
+        self.next_contact_id += 1;
+        contact
     }
 
-    /// Iterates over all held fingers.
-    pub fn held_fingers(&self) -> impl Iterator<Item = Finger> + '_ {
-        self.fingers.keys().cloned()
+    /// Iterates over all held contacts.
+    pub fn held_contacts(&self) -> impl Iterator<Item = Contact> + '_ {
+        self.contacts.keys().cloned()
     }
 
-    /// Iterates over all fingers that were just pressed.
-    pub fn pressed_fingers(&self) -> impl Iterator<Item = Finger> + '_ {
-        let last_fingers = self.last_fingers.keys().cloned().collect::<HashSet<_>>();
-        self.fingers
+    /// Iterates over all contacts that were just pressed.
+    pub fn pressed_contacts(&self) -> impl Iterator<Item = Contact> + '_ {
+        let last_contacts = self.last_contacts.keys().cloned().collect::<HashSet<_>>();
+        self.contacts
             .keys()
             .cloned()
-            .filter(move |finger| !last_fingers.contains(finger))
+            .filter(move |contact| !last_contacts.contains(contact))
     }
 
-    /// Iterates over all fingers that were just released.
-    pub fn released_fingers(&self) -> impl Iterator<Item = Finger> + '_ {
-        let fingers = self.fingers.keys().cloned().collect::<HashSet<_>>();
-        self.last_fingers
+    /// Iterates over all contacts that were just released.
+    pub fn released_contacts(&self) -> impl Iterator<Item = Contact> + '_ {
+        let contacts = self.contacts.keys().cloned().collect::<HashSet<_>>();
+        self.last_contacts
             .keys()
             .cloned()
-            .filter(move |finger| !fingers.contains(finger))
+            .filter(move |contact| !contacts.contains(contact))
     }
 
-    /// Gets the position of a finger, or None if the finger was already lifted from the screen.
-    pub fn finger_position(&self, finger: Finger) -> Option<(f64, f64)> {
-        self.fingers.get(&finger).map(|v| (*v).into())
+    /// Gets the position of a contact, or None if the contact was already lifted from the screen.
+    pub fn contact_position(&self, contact: Contact) -> Option<(f64, f64)> {
+        self.contacts.get(&contact).map(|v| (*v).into())
     }
 
     pub(crate) fn handle_touch_start(&mut self, id: u64, location: PhysicalPosition<f64>) {
-        let finger = self.next_finger();
-        self.ids_to_fingers.insert(id, finger);
-        self.fingers.insert(finger, location);
+        let contact = self.next_contact();
+        self.ids_to_contacts.insert(id, contact);
+        self.contacts.insert(contact, location);
     }
 
     pub(crate) fn handle_touch_move(&mut self, id: u64, location: PhysicalPosition<f64>) {
-        self.fingers.insert(self.ids_to_fingers[&id], location);
+        self.contacts.insert(self.ids_to_contacts[&id], location);
     }
 
     pub(crate) fn handle_touch_end(&mut self, id: u64) {
-        self.fingers.remove(&self.ids_to_fingers[&id]);
-        self.ids_to_fingers.remove(&id);
+        self.contacts.remove(&self.ids_to_contacts[&id]);
+        self.ids_to_contacts.remove(&id);
     }
 
     fn update(&mut self) {
-        self.last_fingers.clone_from(&self.fingers);
+        self.last_contacts.clone_from(&self.contacts);
     }
 }
 
