3a6d51200c0c1b15b5e00bd805ac1b29ceb54e26
coevolutions <156261562+coevolutions@users.noreply.github.com>
2024-10-02 10:54:43 -0700

    clean up native::read

--- src/file/native.rs
+++ src/file/native.rs
@@ -2,16 +2,8 @@ use std::io::ErrorKind;
 
 use super::Error;
 
-fn convert_error(e: std::io::Error) -> Error {
-    if e.kind() == ErrorKind::NotFound {
-        Error::NotFound
-    } else {
-        Error::Other(e.into())
-    }
-}
-
 pub async fn read(path: &str) -> Result<Vec<u8>, Error> {
-    let fut = async {
+    Ok(async {
         #[cfg(feature = "tokio")]
         {
             return tokio::fs::read(path).await;
@@ -27,6 +19,13 @@ pub async fn read(path: &str) -> Result<Vec<u8>, Error> {
             _ = path;
             panic!("no async runtime available!");
         }
-    };
-    Ok(fut.await.map_err(convert_error)?)
+    }
+    .await
+    .map_err(|e| {
+        if e.kind() == ErrorKind::NotFound {
+            Error::NotFound
+        } else {
+            Error::Other(e.into())
+        }
+    })?)
 }
