65386fa769d5c3c1866572d8e03880bf64b4f3d4
coevolutions <156261562+coevolutions@users.noreply.github.com>
2024-10-30 10:41:48 -0700

    add helper math functions

--- src/math.rs
+++ src/math.rs
@@ -1,10 +1,25 @@
-//! Various math types.
+//! Various math types and shorthand helpers.
 //!
-//! These are just reexported from `glam`.
+//! These are just reexported from `glam`, along with some helper functions.
 
 pub use glam::*;
 
 /// Creates a translation matrix to (x, y).
 pub fn translation(x: f32, y: f32) -> Affine2 {
     glam::Affine2::from_translation(Vec2::new(x, y))
+}
+
+/// Creates a scaling matrix by (sx, sy).
+pub fn scale(sx: f32, sy: f32) -> Affine2 {
+    glam::Affine2::from_scale(Vec2::new(sx, sy))
+}
+
+/// Creates a rotation matrix by theta.
+pub fn rotate(theta: f32) -> Affine2 {
+    glam::Affine2::from_angle(theta)
+}
+
+/// Creates a uniform matrix by (s, s).
+pub fn uniform_scale(s: f32) -> Affine2 {
+    glam::Affine2::from_scale(Vec2::new(s, s))
 }
