30d8e4cdcb6f04db3fe20f0b9715df67c99c2f03
coevolutions <156261562+coevolutions@users.noreply.github.com>
2024-11-05 10:31:53 -0800

    don't allow use of urls in file::web::read

--- src/file/web.rs
+++ src/file/web.rs
@@ -3,6 +3,11 @@ use gloo_net::http::Request;
 use super::Error;
 
 pub async fn read(path: &str) -> Result<Vec<u8>, Error> {
+    if url::Url::parse(path).is_ok() {
+        // Don't allow use of URLs.
+        return Err(Error::NotFound);
+    }
+
     let resp = Request::get(&path)
         .send()
         .await
